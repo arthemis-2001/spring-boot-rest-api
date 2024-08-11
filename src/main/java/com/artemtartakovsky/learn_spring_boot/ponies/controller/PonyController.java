be65d4c1c86598aa9356fc96f9d322b79428ecf3
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-08-11 20:40:31 +0200

    Added PUT and DELETE routes

--- src/main/java/com/artemtartakovsky/learn_spring_boot/ponies/controller/PonyController.java
+++ src/main/java/com/artemtartakovsky/learn_spring_boot/ponies/controller/PonyController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +21,14 @@ public class PonyController {
 	@Autowired
 	private PonyRepository repository;
 
-	// http://localhost:8080/ponies
+	// GET - http://localhost:8080/ponies
 	@GetMapping("/ponies")
 	public List<Pony> getAllPonies() {
 //		return Arrays.asList(new Pony(1, "Rainbow Dash", "Pegasus"), new Pony(2, "Pinkie Pie", "Earth Pony"));
 		return repository.findAll();
 	}
 
-	// http://localhost:8080/ponies/{id}
+	// GET - http://localhost:8080/ponies/{id}
 	@GetMapping("/ponies/{id}")
 	public Pony getPony(@PathVariable long id) {
 		Optional<Pony> pony = repository.findById(id);
@@ -37,9 +39,21 @@ public class PonyController {
 		return pony.get();
 	}
 
-	// http://localhost:8080/ponies
+	// POST - http://localhost:8080/ponies
 	@PostMapping("/ponies")
 	public void createPony(@RequestBody Pony pony) {
 		repository.save(pony);
+	}
+
+	// PUT - http://localhost:8080/ponies/{id}
+	@PutMapping("/ponies/{id}")
+	public void updatePony(@PathVariable long id, @RequestBody Pony pony) {
+		repository.save(pony);
+	}
+
+	// DELETE - http://localhost:8080/ponies/{id}
+	@DeleteMapping("/ponies/{id}")
+	public void deletePony(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
